9a3b4792f5304ac3c71960e43aaeb16eff16edef
lep828 <lep828@gmail.com>
2016-09-28 16:35:46 +0100

    trying to refactor board angular style

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -9,19 +9,32 @@
     var vm = this;
 
     // Variables
-    // win conditions
+      // Tiles
+    vm.tiles         = [0,1,2,3,4,5,6,7,8];
+      // Squares
+    vm.squares       = [0,1,2,3,4,5,6,7,8];
+      // win conditions
     vm.winConditions = [14,77,194, // Horizontals
                         66,93,126, // Verticals
                         83,107];   // Diagonals
       // turn count
-    vm.turnCounter = 0;
+    vm.turnCounter  = 0;
       // players moves
     vm.player1Moves = [];
     vm.player2Moves = [];
+      // players wins
+    vm.player1Wins  = [];
+    vm.player2Wins  = [];
+
+    // Viewmodel Functions
+    vm.getMove      = getMove;
 
     // Functions
-      // build board
+      // reset/build board
       // player move
+    function getMove(){
+      console.log($event.target);
+    }
       // move board (to the subsection corresponding)
       // turn counter
       // check win
