cf61058178e6abae22909ad215034e361b23e8f0
lep828 <lep828@gmail.com>
2016-09-28 15:23:06 +0100

    built basic board

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require("express");
+var config  = require("./config/config");
 var app     = express();
 
 // Setting up static folders
 app.use("/", express.static(__dirname + "/public"));
 
-// 
+//
 app.get("/*", function(req, res){
   res.sendFile(__dirname + "/public/views/index.html");
 });
 
-app.listen(3000, function(){
-  console.log("running on port 3000");
+app.listen(config.port, function(){
+  console.log("running on port " + config.port);
 });
--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -6,10 +6,26 @@
     .controller("MainController", MainController);
 
   function MainController(){
-    
-    /*jshint validthis: true */
     var vm = this;
 
-  }
+    // Variables
+    // win conditions
+    vm.winConditions = [14,77,194, // Horizontals
+                        66,93,126, // Verticals
+                        83,107];   // Diagonals
+      // turn count
+    vm.turnCounter = 0;
+      // players moves
+    vm.player1Moves = [];
+    vm.player2Moves = [];
 
+    // Functions
+      // build board
+      // player move
+      // move board (to the subsection corresponding)
+      // turn counter
+      // check win
+      // check draw
+
+  }
 })();
