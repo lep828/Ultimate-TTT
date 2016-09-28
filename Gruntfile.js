1e7db4e8382f52874a546fe063f5c531d9724fd6
lep828 <lep828@gmail.com>
2016-09-28 13:33:29 +0100

    First commit, setting up basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      src: [
+        'src/js/**/*.js',
+        '!src/js/_bower.js'
+      ]
+    },
+    bower_concat: {
+      all: {
+        dest: {
+          'js': 'src/js/_bower.js',
+          // 'css': 'src/scss/_bower.scss'
+        }
+      }
+    },
+    sass: {
+      expanded: {
+        options: { outputStyle: 'expanded' },
+        files: { 'public/css/style.css': 'src/scss/style.scss' }
+      },
+      compressed: {
+        options: { outputStyle: 'compressed' },
+        files: { 'public/css/style.min.css': 'src/scss/style.scss' }
+      }
+    },
+    concat: {
+      jsDist: {
+        src: [
+          'src/js/_bower.js',
+          'src/js/app.js',
+          'src/js/**/*.js'
+        ],
+        dest: 'public/js/app.js'
+      }
+    },
+    uglify: {
+      'public/js/app.min.js': 'public/js/app.js'
+    },
+    watch: {
+      configFiles: {
+        files: ['Gruntfile.js', 'package.json'],
+        options: { reload: true }
+      },
+      scss: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass'],
+        options: { livereload: true }
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['jshint', 'concat', 'uglify'],
+        options: { livereload: true }
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['jshint', 'bower_concat', 'concat', 'sass', 'uglify', 'watch']);
+};
