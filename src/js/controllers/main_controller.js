a6f52deea612f8b658bbeb290db937f1e9c4dd3b
lep828 <lep828@gmail.com>
2016-10-11 21:08:45 +0100

    changed board size

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -15,36 +15,39 @@
     // ************************ //
 
     function init(){
-
       // Game Variables
-      vm.ongoingGame   = true;
-      vm.tiles         = [0,1,2,3,4,5,6,7,8];
-      vm.squares       = [[0,1],[1,4],[2,9],[3,16],[4,25],[5,36],[6,49],[7,64],[8,81]];
-      vm.winConditions = [14,77,194, // Horizontals
-                          66,93,126, // Verticals
-                          83,107];   // Diagonals
+      vm.gameOngoing       = true;
+      vm.gameWon           = false;
+      vm.gameDrawn         = false;
+      vm.tiles             = [0,1,2,3,4,5,6,7,8];
+      vm.squares           = [[0,1],[1,4],[2,9],[3,16],[4,25],[5,36],[6,49],[7,64],[8,81]];
+      vm.tileWinConditions = [14,77,194, // Horizontals
+                              66,93,126, // Verticals
+                              83,107];   // Diagonals
+      vm.gameWinConditions = [[0,1,2],[3,4,5],[6,7,8], // Horizontals
+                              [0,3,6],[1,4,7],[2,5,8], // Verticals
+                              [0,4,8],[2,4,6]];        // Diagonals
 
       // Turn counter variables
-      vm.turnCounter   = 0;
-      vm.player        = "x";
+      vm.turnCounter       = 0;
+      vm.player            = "x";
 
       // Player Variables
-      vm.xMoves        = [[],[],[],[],[],[],[],[],[]];
-      vm.oMoves        = [[],[],[],[],[],[],[],[],[]];
-      vm.xTiles        = [];
-      vm.oTiles        = [];
-      vm.xWins         = [];
-      vm.oWins         = [];
-
+      vm.xMoves            = [[],[],[],[],[],[],[],[],[]];
+      vm.oMoves            = [[],[],[],[],[],[],[],[],[]];
+      vm.xTiles            = [];
+      vm.oTiles            = [];
+      vm.xWins             = [];
+      vm.oWins             = [];
     }
 
     // *************************** //
     // *** Viewmodel Functions *** //
     // *************************** //
 
-    vm.getMove       = getMove;
-    vm.changePage    = changePage;
-    vm.resetGame     = resetGame;
+    vm.getMove             = getMove;
+    vm.changePage          = changePage;
+    vm.resetGame           = resetGame;
 
     // ***************** //
     // *** Functions *** //
@@ -52,7 +55,7 @@
 
     // get player's move
     function getMove(tile, value, $event){
-      while(vm.ongoingGame){
+      while(vm.gameOngoing){
         var square = $event.currentTarget.classList;
         if (checkMove(square, tile)){
           if (vm.player === 'x'){
@@ -138,8 +141,8 @@
           for (var k = j+1; k < array.length; k++){
             total += array[k];
 
-            for (var l = 0; l < vm.winConditions.length; l++){
-              if (total === vm.winConditions[l]){
+            for (var l = 0; l < vm.tileWinConditions.length; l++){
+              if (total === vm.tileWinConditions[l]){
                 console.log("WINNER", vm.player, total);
                 setTile(tile);
               }
@@ -181,19 +184,26 @@
 
     // Checks for a game win
     function checkGameWin(array){
-      var winConditions = [[0,1,2],[3,4,5],[6,7,8],
-                           [0,3,6],[1,4,7],[2,5,8],
-                           [0,4,8],[2,4,6]];
-
-      for (var i = 0; i < winConditions.length; i++){
-        if (array.indexOf(winConditions[i][0]) !== -1 && array.indexOf(winConditions[i][1]) !== -1 && array.indexOf(winConditions[i][2]) !== -1){
-          vm.ongoingGame = false;
-          vm.winner = vm.player;
+      for (var i = 0; i < vm.gameWinConditions.length; i++){
+        if (array.indexOf(vm.gameWinConditions[i][0]) !== -1 && array.indexOf(vm.gameWinConditions[i][1]) !== -1 && array.indexOf(vm.gameWinConditions[i][2]) !== -1){
+          vm.gameOngoing = false;
+          vm.gameWon     = true;
+          vm.winner      = vm.player;
+        } else {
+          checkGameDraw();
         }
       }
     }
 
     // Checks for a draw
+    function checkGameDraw(){
+      console.log("HEREEREER DRAW CHECKERSZZ");
+      var total = vm.xWins.length + vm.oWins.length;
+      if (total === 9 && vm.gameWon === false){
+        vm.gameOngoing = false;
+        vm.gameDrawn   = true;
+      }
+    }
 
     // Reset game
     function resetGame(){
